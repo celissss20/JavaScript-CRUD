6797229d935989fde5f0d6b41de7245390f26848
Celis <dedsa2016@gmail.com>
2023-07-16 12:27:36 -0500

    Codigo de edicion de cliente

--- screens/services/client-service.js
+++ screens/services/client-service.js
@@ -22,11 +22,31 @@ const eliminarCliente =(id) => {
   return fetch(`http://localhost:3000/perfil/${id}`,{
     method: "DELETE"
   })
-}
+};
+
+const detalleCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`).then( respuesta => 
+  respuesta.json()
+  )
+};
+
+const actualizarCliente = (nombre,email,id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`,{
+    method:"PUT",
+    headers:{
+       "Content-Type": "application/json"
+    },
+    body:JSON.stringify({nombre,email}) 
+  })
+  .then((respuesta) => respuesta)
+  .catch(err => console.log(err))
+} 
 
 export const clientServices = {
   listarClientes,
   crearCliente,
   eliminarCliente,
+  detalleCliente,
+  actualizarCliente,
 };
 
